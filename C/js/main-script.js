630487b49f740d46ec088f47b8a7f18dce295f22
Pedro Lameiras <pedrolameiras@tecnico.ulisboa.pt>
2023-06-05 17:17:57 +0100

    Layout

--- C/js/main-script.js
+++ C/js/main-script.js
@@ -2,6 +2,13 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
+var active_camera;
+var cameras = new Array(5);
+var key_press_map = {};
+var dirLight;
+
+var moon;
+
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -9,20 +16,72 @@
 function createScene(){
     'use strict';
 
+    scene = new THREE.Scene();
+
+    scene.background = new THREE.Color(0xeeeeff);
+
+    createMoon();
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
 
+function createCameras() {
+    'use strict'
+    active_camera = 0;
+    createIsometricOrtogonalCamera();
+}
+
+function createIsometricOrtogonalCamera() {
+    'use strict'
+    cameras[0] = new THREE.OrthographicCamera( -window.innerWidth / 32, window.innerWidth / 32, window.innerHeight / 32, -window.innerHeight / 32, 1, 100);
+    cameras[0].position.set(15, 15, 15);
+    cameras[0].rotation.z = 0;
+    cameras[0].lookAt(scene.position);
+}
+
 
 /////////////////////
 /* CREATE LIGHT(S) */
 /////////////////////
 
+function createDirectionalLight() {
+	dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+	dirLight.position.set(30, 30, 60);
+	dirLight.target.position.set(0, 0, 0); //Width and height?
+	dirLight.target.updateMatrixWorld();
+	scene.add(dirLight);
+}
+
+
+
+function createLights(){
+    createDirectionalLight();
+}
+
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+// Cube primitive mesh
+function createSphere(radius, widthSegments, heightSegments) {
+    'use strict';   
+
+    const mySphere = new THREE.SphereGeometry(radius, widthSegments, heightSegments); 
+    const material = new THREE.MeshBasicMaterial({color: 0xffff00}); 
+    const mesh = new THREE.Mesh( geometry, material ); 
+    
+    mesh.position.set(0, 0, 0);
+    
+    return mesh;
+}
+
+function createMoon(){
+    'use strict';
+    moon = createSphere(5, 32, 16);
+
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -62,6 +121,19 @@ function render() {
 function init() {
     'use strict';
 
+    renderer = new THREE.WebGLRenderer({antialias: true})
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCameras();
+    createLights();
+
+    window.addEventListener("resize", onResize);
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
 }
 
 /////////////////////
@@ -86,6 +158,16 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    switch(e.keyCode) {
+    // Camera changes
+    case 49: // 1
+        key_press_map[49] = !key_press_map[49];
+        break;
+    case 68: // D
+    case 100: // d
+        key_press_map[68] = 1;
+        break;
+    }
 }
 
 ///////////////////////
@@ -93,5 +175,7 @@ function onKeyDown(e) {
 ///////////////////////
 function onKeyUp(e){
     'use strict';
+    switch (e.keyCode) {
 
+    }
 }
